6e911c69a3a452c1fced82daffd375c8fb49d29b
Max2 <chigov@gmail.com>
2021-11-11 21:44:17 +0200

    Initial commit

---
+++ app/src/main/java/com/example/timer6/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.timer6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void setStart(View view) {
+    }
+
+    public void setPsuse(View view) {
+    }
+
+    public void setReset(View view) {
+    }
+    //создаем счетчик
+}
